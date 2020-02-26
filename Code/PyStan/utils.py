d9f7c9c25f3bdb9fd197ea1569ff5a8023cc5c1e
tobias-beers <tobias.beers@student.uva.nl>
2020-02-26 22:01:19 +0100

    finished hierarchical model as described by Bishop & Tipping

--- Code/PyStan/utils.py
+++ Code/PyStan/utils.py
@@ -1,15 +1,29 @@
 import pickle
 import pystan
 
-def loadStan(file):
-    try:
-        model = pickle.load(open('pickled_models/'+file+'.pkl', 'rb'))
-        print('Model loaded succesfully from cache.')
-    except:
-        model = pystan.StanModel(file = 'StanModels/'+file+'.stan')
-        print('Model compiled succesfully.')
-        with open('pickled_models/'+file+'.pkl', 'wb') as f:
-            pickle.dump(model, f)
-        print('Model saved succesfully to cache.')
-    
+def loadStan(file, recompile=False, automatic_pickle = True):
+    if recompile:
+        try:
+            model = pystan.StanModel(file = 'StanModels/'+file+'.stan')
+            print('Model compiled succesfully.')
+            with open('pickled_models/'+file+'.pkl', 'wb') as f:
+                pickle.dump(model, f)
+            print('Model saved succesfully to cache.')
+        except:
+            print(file+'.stan not found!')
+    else:
+        try:
+            model = pickle.load(open('pickled_models/'+file+'.pkl', 'rb'))
+            print('Model loaded succesfully from cache.')
+        except:
+            try:
+                model = pystan.StanModel(file = 'StanModels/'+file+'.stan')
+                print('Model compiled succesfully.')
+                if automatic_pickle:
+                    with open('pickled_models/'+file+'.pkl', 'wb') as f:
+                        pickle.dump(model, f)
+                    print('Model saved succesfully to cache.')
+            except:
+                print(file+'.stan and '+file+' both not found!')
+
     return model
